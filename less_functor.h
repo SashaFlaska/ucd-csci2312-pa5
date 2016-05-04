cabcf854aafe31bf7c30abb236ca906d0da5f21a
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-05-04 14:25:33 -0600

    Added files via upload

---
+++ less_functor.h
@@ -0,0 +1,13 @@
+//
+//  less_functor.h
+//  ProgrammingAssignment5
+//
+//  Created by Alexander Flaska on 5/4/16.
+//  Copyright © 2016 Alexander Flaska. All rights reserved.
+//
+
+#ifndef less_functor_h
+#define less_functor_h
+
+
+#endif /* less_functor_h */
