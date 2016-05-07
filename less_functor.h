a9cfda754f9a1c05918966dfa9b9821d397d1633
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-05-07 17:13:14 -0600

    didn't push, had to re-upload

--- less_functor.h
+++ less_functor.h
@@ -9,5 +9,45 @@
 #ifndef less_functor_h
 #define less_functor_h
 
+#include <cstring>
+// git test comment.
+namespace CS2312
+{
+    
+    template <typename T>
+    class less
+    {
+    public:
+        bool operator() (const T &left, const T &right)
+        {
+            return (left < right);
+        }
+    };
+    
+    template <>
+    class less<const char*>
+    {
+    public:
+        bool operator() (const char *left, const char *right)
+        {
+            return (strcmp(left, right) < 0);
+        }
+    };
+}
+
+    template <>
+    class less<std::string>
+    {
+    public:
+        bool operator() (const std::string &left, const std::string &right)
+        {
+            return (left < right);
+        }
+    };
+    
+    
+
 
 #endif /* less_functor_h */
+
+
